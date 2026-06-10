ddaae3c5639a72f38254994c6036e9c30f056154
luis Ortega <ortegaluiskb@gmail.com>
2026-06-09 23:08:24 -0600

    Update timming async

--- cache/timming.py
+++ cache/timming.py
@@ -1,8 +1,16 @@
 # 
+import asyncio
+import time
 
 l1 = 'fastest'
 l2 = 'fast'
 l3 = ['core 1', 'core 2', 'core 3', 'core 4']
 
 def reescalado():
+    time.sleep(4)
     print(l3)
+
+async def running():
+    await reescalado()
+
+asyncio.run(main())
