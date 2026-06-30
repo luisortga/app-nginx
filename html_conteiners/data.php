354f945e638bd581912706170b72571608ba2b1f
luisOrtga <ortegaluiskb@gmail.com>
2026-06-30 14:01:03 -0600

    Add code php

---
+++ html_conteiners/data.php
@@ -0,0 +1,11 @@
+// Commentary in php is done using two types of comments: single-line comments and multi-line comments.
+
+# Single-line comments can be created using either `//` or `#`. For example:
+```php
+
+// This is a single-line comment using double slashes
+
+function exampleFunction() {
+    // This function does something
+    echo "Hello, World!";
+}
