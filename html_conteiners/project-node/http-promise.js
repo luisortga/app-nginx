dec58d5a380e93be704f59b9b1ca8ed6592c84d2
luisOrtga <ortegaluiskb@gmail.com>
2026-07-04 23:46:32 -0600

    Upgrade http promise all

--- html_conteiners/project-node/http-promise.js
+++ html_conteiners/project-node/http-promise.js
@@ -2,10 +2,13 @@ const http = require('node:http') // protocolo HTTP
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-    console.log('request received')
+const processRequest = (req, res) => {
+    console.log('request received: ', req.url)
     res.end('Hello World')
-})
+}
+
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`server listening on port http://localhost:${desiredPort}`)
