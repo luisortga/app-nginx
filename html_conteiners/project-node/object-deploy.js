d401613f24ffd5b91dc0d0edcf003e1824a89967
luisOrtga <ortegaluiskb@gmail.com>
2026-07-07 20:57:10 -0600

    Upgrade object deploy

--- html_conteiners/project-node/object-deploy.js
+++ html_conteiners/project-node/object-deploy.js
@@ -2,5 +2,16 @@
 
 const prometeo = new Promise((resolve, reject) => {
     //
+    setTimeout(() => {
+        resolve('Successful')
+        reject('exit')
+    }, 6000)
+})
 
+prometeo.then(resol => {
+    console.log(resol)
+}).catch(reject => {
+    console.error(reject)
+}).finally(() => {
+    console.log('process final')
 })
