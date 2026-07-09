63ca3e88c6c22ce05263f01395146f20a8e75bd9
luisOrtga <ortegaluiskb@gmail.com>
2026-07-08 21:56:11 -0600

    update validate error resolved

--- html_conteiners/project-node/validate-errors.js
+++ html_conteiners/project-node/validate-errors.js
@@ -1,5 +1,13 @@
+
 const validateUser = ({name, age, email}) => {
     if (!name) throw new Error('name is required')
     if (!age) throw new Error('name is required')
     if (!email) throw new Error('name is required')
     }
+
+
+try {
+    validateUser()
+} catch (e) {
+console.log(typeof e)
+}
