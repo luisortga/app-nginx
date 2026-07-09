f30e733931a53e54143f9cf949b3c6a98d4530b3
luisOrtga <ortegaluiskb@gmail.com>
2026-07-08 22:56:19 -0600

    Update validate now

--- html_conteiners/project-node/validate-errors.js
+++ html_conteiners/project-node/validate-errors.js
@@ -1,8 +1,27 @@
+const { retry } = require("rxjs")
+
 const validateUser = ({ name, age, email }) => {
   if (!name) throw new Error("name is required")
   if (!age) throw new Error("name is required")
   if (!email) throw new Error("name is required")
+
+  // call to data base
+
+  throw new ConnectionError("database is not available")
 }
+
+class ConnectionError extends Error {}
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = "ValidationError"
+  }
+}
+
+const name = "luis"
+const age = 28
+const email = null
 
 try {
   validateUser()
