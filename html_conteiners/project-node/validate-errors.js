23987670b9985ba4c9898111a645aff78a7b3a57
luisOrtga <ortegaluiskb@gmail.com>
2026-07-08 22:03:29 -0600

    add prettierrc

--- html_conteiners/project-node/validate-errors.js
+++ html_conteiners/project-node/validate-errors.js
@@ -1,13 +1,11 @@
-
-const validateUser = ({name, age, email}) => {
-    if (!name) throw new Error('name is required')
-    if (!age) throw new Error('name is required')
-    if (!email) throw new Error('name is required')
-    }
-
+const validateUser = ({ name, age, email }) => {
+  if (!name) throw new Error("name is required")
+  if (!age) throw new Error("name is required")
+  if (!email) throw new Error("name is required")
+}
 
 try {
-    validateUser()
+  validateUser()
 } catch (e) {
-console.log(typeof e)
+  console.log(err instanceof Error)
 }
