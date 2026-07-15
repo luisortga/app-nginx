8205f0e4f41568e65672bcb7513deb32050557fd
luisOrtga <ortegaluiskb@gmail.com>
2026-07-14 23:01:34 -0600

    subscribers resolved in new meta class

--- html_conteiners/project-node/subcribers.mjs
+++ html_conteiners/project-node/subcribers.mjs
@@ -1,15 +1,19 @@
-// init
+import { from } from "rxjs"
+import { map, catchError } from "rxjs/operators"
+import fetch from "node-fetch"
 
-export class Subscribers {
-  constructor(actions, final) {
-    /* */
-    this.actions = actions
-    this.final = final
-  }
-
-  toString() {
-    return ""
-  }
+function getUser(id) {
+  return from(
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) =>
+      res.json(),
+    ),
+  ).pipe(
+    map((user) => ({ id: user.id, name: user.name, email: user.email })),
+    catchError((err) => {
+      console.error("Error fetching user", err)
+      return []
+    }),
+  )
 }
-// pending
-const sub = new Subscribers("Observer delay", 16)
+
+getUser(1).subscribe((user) => console.log("Usuario:", user))
