ffd9708f00577081c18626c6d57ec1a006377869
luisOrtga <ortegaluiskb@gmail.com>
2026-07-18 10:51:36 -0600

    Update with class book js

--- book-js/intervalos.js
+++ book-js/intervalos.js
@@ -1,5 +1,14 @@
-const test = setInterval(() => {
-  let counter = 0
+let counter = 0
 
-  console.log('Started of Interval')
-})
+console.log('Started of Interval')
+
+const test = setInterval(() => {
+  counter++
+  console.log(`Interval counter: ${counter}`)
+  if (counter === 8) {
+    console.log(`${counter} is final`)
+    console.log(`Sucessful async`)
+
+    clearInterval(test)
+  }
+}, 1000)
