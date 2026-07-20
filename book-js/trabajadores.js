a9d2ce118dfedc4074d9fbdd723753cee61bccb8
luisOrtga <ortegaluiskb@gmail.com>
2026-07-19 21:04:52 -0600

    Update workers js

--- book-js/trabajadores.js
+++ book-js/trabajadores.js
@@ -1,1 +1,11 @@
-// prueba para que se retire con git stash
+// workers, multi threads
+
+const { parentPort } = require('worker_threads')
+
+let total = 0
+
+for (let i = 0; i < 10_000_000_000; i++) {
+  total += i
+}
+
+parentPort.postMessage(total)
