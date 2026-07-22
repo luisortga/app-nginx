3d2daf66eb56bf0338c3d4d554c399d03f8224bb
luisOrtga <ortegaluiskb@gmail.com>
2026-07-21 22:15:43 -0600

    Update method arrays

--- book-js/metodos-array.js
+++ book-js/metodos-array.js
@@ -1,3 +1,11 @@
 const numeros = [1, 2, 3, 4]
 const duplicados = numeros.map((x) => x * 2)
 // [2, 4, 6, 8]
+
+// filter
+
+const numeros = [1, 2, 3, 4, 5]
+const pares = numeros.filter((x) => x % 2 === 0)
+// [2, 4]
+
+//
