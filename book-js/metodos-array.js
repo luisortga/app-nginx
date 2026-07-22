b14dee4e99146f36563e7ca7a08664521ad5b016
luisOrtga <ortegaluiskb@gmail.com>
2026-07-21 22:16:49 -0600

    Complement method arrays

--- book-js/metodos-array.js
+++ book-js/metodos-array.js
@@ -8,4 +8,14 @@ const numeros = [1, 2, 3, 4, 5]
 const pares = numeros.filter((x) => x % 2 === 0)
 // [2, 4]
 
-//
+// reduce
+
+const numeros = [1, 2, 3, 4]
+const suma = numeros.reduce((acumulador, numero) => acumulador + numero, 0)
+// 10
+
+// Desglose:
+// acumulador = 0, numero = 1 → acumulador = 1
+// acumulador = 1, numero = 2 → acumulador = 3
+// acumulador = 3, numero = 3 → acumulador = 6
+// acumulador = 6, numero = 4 → acumulador = 10
